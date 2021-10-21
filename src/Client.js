876fbaaadeb61802deab22c9c51e108c6b1a6c5e
bmwasonga <bmwasonga@gmail.com>
2021-10-21 23:06:39 +0300

    adding auth head

--- src/Client.js
+++ src/Client.js
@@ -8,7 +8,7 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem(AUTH_TOKEN);
 
   return {
     headers: {
